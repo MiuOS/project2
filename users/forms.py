eebb80642a0e3d622c9f9bfc3d9ee438c2cf8f62
Rajdun <=>
2023-11-17 18:15:43 +0100

    navbar

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
@@ -7,6 +7,5 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = UserCreationForm.Meta.fields
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+class LoginForm(AuthenticationForm):
+    pass
--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import CustomUserCreationForm, LoginForm
 
 def register_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
@@ -14,8 +18,11 @@ def register_request(request):
     return render(request, "users/register.html", {"form": form})
 
 def login_request(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
-        form = LoginForm(request.POST)
+        form = LoginForm(data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -30,6 +37,7 @@ def login_request(request):
         form = LoginForm()
     return render(request, "users/login.html", {"form": form})
 
+@login_required(login_url="login")
 def logout_request(request):
     logout(request)
-    return redirect("home")
+    return redirect("register")
