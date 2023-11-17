90de82fa1d7a2f7e0b032e5b275795ede29c4b24
Rajdun <=>
2023-11-17 19:46:09 +0100

    panel admina

--- init.sh
+++ init.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-# Czekaj na połączenie z bazą danych (opcjonalnie)
+# Ustaw czas oczekiwania na 10 sekund
+max_attempts=10
+attempts=0
+
+# Czekaj na połączenie z bazą danych
+until curl -s db:5432 || [ $attempts -eq $max_attempts ]; do
+  attempts=$((attempts+1))
+  echo "Czekam na połączenie z bazą danych... Próba $attempts z $max_attempts"
+  sleep 1
+done
+
+# Sprawdź, czy połączenie zostało nawiązane
+if [ $attempts -eq $max_attempts ]; then
+  echo "Nie udało się nawiązać połączenia z bazą danych. Kontynuuję..."
+else
+  echo "Połączenie z bazą danych nawiązane."
+fi
 
 # Tworzenie migracji
 python manage.py makemigrations
 
 # Wykonywanie migracji
 python manage.py migrate
+
+# utwórz superusera Django jeśli nie istnieje
+echo "from users.models import CustomUser as User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell
 
 # Uruchom serwer Django
 python manage.py runserver 0.0.0.0:8000
