37ff7d099d7bacac9bef89f0b5791511f741c5a1
Rajdun <=>
2024-01-09 15:26:53 +0100

    sqlite

--- project2/settings.py
+++ project2/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = "project2.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbname',  # Your PostgreSQL database name
-        'USER': 'dbuser',  # Your PostgreSQL user
-        'PASSWORD': 'dbpass',  # Your PostgreSQL password
-        'HOST': 'db',  # Name of your docker-compose service for PostgreSQL
-        'PORT': '5432',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
